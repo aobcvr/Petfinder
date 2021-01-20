ac142b92dceb1af1b41ae05e2d0ee898ca701c3b
Paveligin <dkdjjdkd@gmail.com>
2021-01-20 20:54:51 +0700

    create comment_edit

--- rest/serializer/comment_edit.py
+++ rest/serializer/comment_edit.py
@@ -11,13 +11,12 @@ class CommentEdit(serializers.Serializer):
     def comment_edit(self, request):
         validated_data = self.validated_data
         comment_object = Comment.objects.get(id=validated_data['object_id'])
-        print(comment_object.date_create.time())
-        print(datetime.datetime.now(tz=None).time())
         time_difference = datetime.datetime.now() - datetime.timedelta(minutes=15)
-        print(time_difference)
         if request.user == comment_object.user and comment_object.date_create.time() > time_difference.time():
             comment_object.comment = validated_data['edit_comment']
             comment_object.save()
             return {'status': 'comment was edited'}
         else:
             return {'status': 'it is not your comment'}
+
+    def del
